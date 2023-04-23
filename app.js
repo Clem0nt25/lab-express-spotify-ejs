2330d0234c08f9780eea50a73dffe4a914321b6f
Clemens Marckwardt <clemens.marckwardt@gmail.com>
2023-04-23 20:57:03 +0200

    first iteration

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 
 // require spotify-web-api-node package here:
-
+const SpotifyWebApi = require('spotify-web-api-node')
 const app = express()
 
 app.use(expressLayouts)
@@ -13,7 +13,36 @@ app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error))
 
-// Our routes go here:
+
+// GET Route to create one recipe
+app.get('/', (request, response) => {
+    response.render('home')
+  })
+
+app.get('/artist-search', (request, response) => {
+    const searchTerm = request.query.search
+
+    spotifyApi
+    .searchArtists(searchTerm)
+    .then(data => {
+        console.log('The received data from the API: ', data.body)
+        response.send(data)
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err))
+
+
+})
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'))
